32e468ecbb329692bb759090671858ca502535a8
dougvader <30653340+dougvader@users.noreply.github.com>
2020-05-15 14:24:18 +1000

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,13 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.js'
+];
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
@@ -9,3 +19,14 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
