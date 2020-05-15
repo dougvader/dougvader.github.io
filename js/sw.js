854a9a7da52fd0271ab3647a4c5d3829a362d38a
dougvader <30653340+dougvader@users.noreply.github.com>
2020-05-15 15:10:22 +1000

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,17 +1,24 @@
 var CACHE_NAME = 'my-site-cache-v1';
 
 self.addEventListener('install', function(event) {
-  console.log('event listener triggered');
   event.waitUntil(
     caches.open('my-site-cache-v1').then(function(cache) {
-      console.log('opening existing cache');
-      console.log('adding urls to cache');
       return cache.addAll([
         '/css/styles.css',
         '/js/dbhelper.js',
         '/js/main.js',
         '/js/restaurant_info.js',
-        '/data/restaurants.json'
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
       ]);
     }));
 });
